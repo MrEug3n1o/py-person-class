3fe8c0e4975233ae966794edb3a4375197bf58da
mr_eugene <roberttimethone@gmail.com>
2025-07-05 01:29:17 +0200

    Solution1

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
 
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for person_data in people:
+        name = person_data["name"]
+        age = person_data["age"]
+        Person(name, age)
+
+    for person_data in people:
+        name = person_data["name"]
+        person_instance = Person.people[name]
+
+        for role in ("wife", "husband"):
+            if role in person_data and person_data[role] is not None:
+                partner_name = person_data[role]
+                partner_instance = Person.people[partner_name]
+                setattr(person_instance, role, partner_instance)
+
+    return list(Person.people.values())
+
